4a576db0a769ec3392c1e92e40dd34c0176727c6
mttt <afds@dafdsa.com>
2020-07-14 01:44:45 -0400

    added documentation

--- app/Http/Controllers/NotificationController.php
+++ app/Http/Controllers/NotificationController.php
@@ -8,6 +8,12 @@ use Illuminate\Routing\Controller as BaseController;
 
 class NotificationController extends BaseController
 {
+	
+	 /**
+     * Basic REST functions for notifications
+     *
+     * 
+     */
     public function index()
     {
         return Notification::all();
@@ -15,10 +21,8 @@ class NotificationController extends BaseController
  
     public function show($id)
     {
-		//print "test $id";
 		//$res = Notification::find($id) ;
-		
-		
+
         return Notification::find($id);
     }
 
@@ -43,8 +47,61 @@ class NotificationController extends BaseController
         return 204;
     }
 	
+	/**
+     * creates a user and password grant oauth client  
+	 * the email, password, clientID and secret can be passed to 
+	 * /oauth/token to get the authentication token 
+     *
+     * 
+     */
+	public function registerNotificationUser(Request $request)
+	{
+		$user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+		print $user->id ;
+
+		$clients = new ClientRepository() ;
+		
+		$name = 'Password Grand Client';
+		$client = $clients->createPasswordGrantClient(
+            $user->id, $name, 'http://localhost', 'users'
+        );
+		print "<pre>".print_r($client);
+		
+		$response = new stdClass();		
+		$response->user_id = $user->id;
+		$response->client_id = $client->id;
+		$response->secret = $client->secret;
+		return $response;
+		
+	}
+
 	
-	
+	/**
+     * Displays response from yelp api to demonstrate an api connection. 
+     *
+     * 
+     */
+	public function yelpApi()
+	{
+		print "connecting to api:<br>";
+		
+		$token = 'WlmH5ynCSPZI8lm4zAlNvaDWaG4Y2N28tsoeRijvia7227MzVv1DnZ6fsyyeC2sSxk3nnBW7VPQztP4nMawoCe_Z5I2UunYuQhldK5kWdfCn9e_AGIzlCu7zNdQHX3Yx';
+		$client = new Client();
+
+		$res = $client->request('GET', 'https://api.yelp.com/v3/businesses/tW0Zr4o5cFPR7bNxiJ4HCw', [
+		'headers' => [
+		'Authorization' => 'Bearer ' . $token,   
+		'Accept' => 'application/json',
+		'Content-type' => 'application/json'
+		]]);
+
+		print "<pre>".json_encode(json_decode($res->getBody()), JSON_PRETTY_PRINT);
+
+	}
 	
 	
 }
--- app/Notification.php
+++ app/Notification.php
@@ -6,7 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Notification extends Model
 {
-    //
+    
+	/**
+     * Class for notification object that goes with the notifications table
+     *
+     * 
+     */
+	
 	protected $fillable = [
         'user_id', 
 		'status', 
